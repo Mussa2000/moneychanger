ef7a9317272c78e0fd874a2f3804205376e377a7
mist_coat <couragemhute21@gmail.com>
2024-04-28 14:09:34 +0200

    modify dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,6 +9,7 @@ from accounts.models.user import CustomUser
 from dashboard.helpers.province_stats import ProvinceStats
 from django.db.models import Sum
 
+from shield.models.alert import Alert
 from shield.models.folder import Folder
 
 class DashboardListView(LoginRequiredMixin,TemplateView):   
@@ -21,10 +22,12 @@ class DashboardListView(LoginRequiredMixin,TemplateView):
         folders = Folder.objects.all()
         user_count = CustomUser.objects.all().count() or 0
         folder_count = Folder.objects.count() or 0
+        alert_count = Alert.objects.count() or 0
 
         context['folders'] = folders
         context['user_count'] = user_count 
         context['folder_count'] = folder_count 
+        context['alert_count'] = alert_count 
 
         return context
     
