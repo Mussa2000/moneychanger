1b225438b864b8505f4ad24fb4e79009851ac2c8
mist_coat <couragemhute21@gmail.com>
2024-04-28 06:54:09 +0200

    dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,40 +9,22 @@ from accounts.models.user import CustomUser
 from dashboard.helpers.province_stats import ProvinceStats
 from django.db.models import Sum
 
+from shield.models.folder import Folder
+
 class DashboardListView(LoginRequiredMixin,TemplateView):   
     login_url = reverse_lazy('account_login')
     template_name = 'dashboard/dashboard.html'
     
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     user = self.request.user
-    #     farmers_count = CustomUser.objects.filter(is_farmer=True).count() or 0
-    #     bank_count = Bank.objects.count() or 0
-    #     province_count = Province.objects.count() or 0
-    #     paid_amount = Transaction.objects.filter(status='paid').aggregate(total_paid_amount=Sum('amount_paid'))['total_paid_amount'] or 0.00
-    #     partially_paid_amount= Transaction.objects.filter(status='pending', amount_paid__gt=0).aggregate(total_partially_paid_amount=Sum('amount_paid'))['total_partially_paid_amount'] or 0.00
-    #     province_stats = ProvinceStats.get_stats()
-        
-    #     #is farmer stats
-    #     farmer_paid_amount = Transaction.objects.filter(user=user, status='paid').aggregate(total_paid_amount=Sum('amount_paid'))['total_paid_amount'] or 0.00
-    #     farmer_partially_paid_amount = Transaction.objects.filter(user=user, status='pending', amount_paid__gt=0).aggregate(total_partially_paid_amount=Sum('amount_paid'))['total_partially_paid_amount'] or 0.00
-    #     farmer_invoice_count = Transaction.objects.filter(user=user).count()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        folders = Folder.objects.all()
+        user_count = CustomUser.objects.all().count() or 0
+        folder_count = Folder.objects.count() or 0
 
-    #     if user.is_farmer:
-    #         transactions = Transaction.objects.filter(user=user)
-    #     else:
-    #         transactions = Transaction.objects.all()
-        
-    #     context['transactions'] = transactions
-    #     context['farmers_count'] = farmers_count
-    #     context['bank_count'] = bank_count 
-    #     context['paid_amount'] = paid_amount 
-    #     context['partially_paid_amount'] = partially_paid_amount 
-    #     context['province_count'] = province_count
-    #     context['province_stats'] =  province_stats
-        
-    #     context['farmer_paid_amount'] = farmer_paid_amount 
-    #     context['farmer_partially_paid_amount'] = farmer_partially_paid_amount
-    #     context['farmer_invoice_count'] = farmer_invoice_count 
-    #     return context
+        context['folders'] = folders
+        context['user_count'] = user_count 
+        context['folder_count'] = folder_count 
+
+        return context
     
